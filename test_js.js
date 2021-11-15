064a84df91ba9a3030b021f2395e8515d4df96d2
jujudecodes <93547090+jujudecodes@users.noreply.github.com>
2021-11-15 17:02:44 +0100

    new page

--- test_js.js
+++ test_js.js
@@ -2,16 +2,7 @@
 
 $(document).ready(function(){
 
-	var x = 0
-	var x = prompt("What's the value of x ?");
-
-
-function C(x){
-x = x*x;
-}
-
-	console.log("x value is :",x);
-
-});
-
-
+$('button').click(function(){
+alert("bonjour");
+})
+})
---
+++ video_js.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function(){
+
+
+$('video')[0]
+$('Play').click(function(){
+
+})
+
+$('Pause').click(function(){
+
+})
+
+$('Stop').click(function(){
+
+})
+
+$('Slow Motion').click(function(){
+
+})
+})
